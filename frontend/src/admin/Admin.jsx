ca0cf1ed77cd674cb2130d06519e75ae9dc59cbf
Youssef <youssefkh.tv2@gmail.com>
2024-10-14 11:03:47 +0100

    adding somthing

--- frontend/src/admin/Admin.jsx
+++ frontend/src/admin/Admin.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { SideBar } from '../sideBar/SideBar';
 import { TopHeader } from '../topheader/Topheader';
 import { Products } from './Products';
@@ -7,37 +7,39 @@ import { Header } from '../header/Header';
 export const Admin = () => {
     const [message, setMessage] = useState('');
     const [title, setTitle] = useState('Dashboard');
-    
+    const [action, setAction] = useState('');
 
-    
-    const handleMessageFromChild = (message ) => {
-        setMessage(message);
-        
-       
+    const handleAction = (AddPr) => {
+        setAction(AddPr);  
+        console.log(AddPr);
     };
-    
+
+    useEffect(() => {
+        console.log('Updated action:', action);
+    }, [action]);
+
+    const handleMessageFromChild = (message) => {
+        setMessage(message);
+    };
 
     const handleUpdateTitleFromChild = (title) => {
         setTitle(title);
-        console.log(title)
     };
 
     const handleSideBarClick = (message, title) => {
         handleMessageFromChild(message);
         handleUpdateTitleFromChild(title);
-        handlethis(state) ;
-        
     };
 
     return (
-        <section className='flex justify-center items-center h-full w-full '>
-            
+        <section className='flex justify-center items-center h-full w-full'>
             <div className='h-5/6 flex justify-center items-center w-11/12 mt-14'>
                 <SideBar onSendMessage={handleSideBarClick} />
-                <Header/>
+                <Header />
                 <div className="h-full rounded-r-xl w-5/6 border-t-2 border-r-2 border-b-2 border-slate-600">
                     <TopHeader PageTitle={title || "Dashboard"} />
-                    {title == "Products" &&< Products/>}
+                    {title === "Products" && <Products onAddPr={handleAction} />}  {/* Corrected prop name */}
+                    {action === "showAddPage" && <AddPrPage/>}
                 </div>
             </div>
         </section>
