03ff5f422544831f75410a8e14599130556afefd
Omar Bannour <bannouromar54@gmail.com>
2024-10-16 02:36:53 +0200

    header

--- frontend/src/hero/Hero.jsx
+++ frontend/src/hero/Hero.jsx
@@ -4,11 +4,12 @@ import img1 from "../assets/hero.svg.jpg"; // Adjust the path as necessary
 export const Hero = () => {
   return (
     <div className="hero min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="cadre p-20 shadow-lg rounded-lg flex items-center space-x-20 bg-transparente">
+      <div className="cadre p-24 shadow-lg rounded-lg flex items-center space-x-20 bg-transparente">
         {/* Left-side text */}
         <div className="hero-text">
-          <h1 className="text-4xl font-bold">Welcome to Our Website</h1>
-          <p className="mt-4 text-lg">Here is some information about our services...</p>
+          <h3 className="text-2xl ">----- OUR BESTSELLERS</h3>
+          <h1 className="text-5xl font-extrabold tracking-wide text-center p-6 b rounded-lg ">Latest Arrivals</h1>
+          <h3 className="text-2xl "> SHOP NOW------</h3>
         </div>
         
         {/* Right-side image */}
