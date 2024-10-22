62bad7718378aa0605e479b5281b6035f2c625ec
Omar Bannour <bannouromar54@gmail.com>
2024-10-22 15:39:12 +0200

    about

--- frontend/src/components/about/About.jsx
+++ frontend/src/components/about/About.jsx
@@ -4,41 +4,66 @@ import { Footer } from "../footer/Footer";
 import { Header } from "../header/Header";
 
 export const About = () => {
- 
   return (
-    
-    <div className="h-full mt-60 flex flex-col items-center justify-center bg-gray-100 py-20 select-none">
-      <Header/>
-      <h1 className="text-2xl font-bold text-gray-500 mb-8 mt-8">ABOUT US</h1>
+    <div className="min-h-screen flex flex-col items-center justify-center  py-20">
+      <Header />
+      <h1 className="text-2xl font-bold text-gray-500 mt-8">ABOUT US</h1>
       <div className="flex flex-col lg:flex-row items-center justify-between max-w-6xl mx-auto mt-10 mb-10">
         <img
-          className="w-auto lg:w-1/2 h-auto object-cover rounded-lg mb-10 mt-10"
+          className="w-auto lg:w-1/3 h-auto object-cover rounded-lg mb-10 mt-10 "
           src={about}
           alt="About Us"
         />
-        <div className="w-64 h-auto lg:w-1/2 lg:pl-10 text-gray-700 mt-6 lg:mt-0 mt-10 mb-10">
-          <p className="text-lg ">
+        <div className="w-64 h-auto lg:w-1/2 lg:pl-10 text-gray-700 mt-12 lg:mt-0 mt-10 mb-10 mr-40">
+          <p>
             Forever was born out of a passion for innovation and a desire to
             revolutionize the way people shop online. Our journey began with a
             simple idea: to provide a platform where customers can easily
             discover, explore, and purchase a wide range of products from the
-            comfort of their homes. <br /> Since our inception, we've worked tirelessly
-            to curate a diverse selection of high-quality products that cater to
-            every taste and preference.From fashion and beauty to electronics
-            and home essentials, we offer an extensive collection sourced from
-            trusted brands and suppliers. <br />  <p className="text-lg font-semibold mt-4">Our Mission:</p> <br /> Our mission at Forever is
-            to empower customers with choice, convenience, and confidence. We're
-            dedicated to providing a seamless shopping experience that exceeds
-            expectations, from browsing and ordering to delivery and beyond.
+            comfort of their homes. <br /> Since our inception, we've worked
+            tirelessly to curate a diverse selection of high-quality products
+            that cater to every taste and preference.From fashion and beauty to
+            electronics and home essentials, we offer an extensive collection
+            sourced from trusted brands and suppliers. <br />{" "}
+            <p className="text-lg font-semibold mt-4">Our Mission:</p> <br />{" "}
+            Our mission at Forever is to empower customers with choice,
+            convenience, and confidence. We're dedicated to providing a seamless
+            shopping experience that exceeds expectations, from browsing and
+            ordering to delivery and beyond.
           </p>
-
-      
         </div>
       </div>
-      
-      <Footer/>
+      <h1 className="font-blond col-gray-600 text-2xl mb-20 ">
+        WHY CHOOOSE US
+      </h1>
+      <table className="table-auto border-collapse border border-gray-300 w-200 mb-20 ">
+        <tr>
+          <td className="border border-gray-500 px-4 py-2 font-bold">
+            Quality Assurance:
+            <p  >
+              We meticulously select and vet each product to ensure it meets our
+              stringent quality standards.
+            </p>
+          </td>
+          <td className="border border-gray-500 px-4 py-2 font-bold">
+            Convenience:
+            <p>
+              With our user-friendly interface and hassle-free ordering process,
+              shopping has never been easier.
+            </p>
+          </td>
+          <td className="border border-gray-500 px-4 py-2 font-bold">
+            Exceptional Customer Service:
+            <p>
+              Our team of dedicated professionals is here to assist you,
+              ensuring your satisfaction is our top priority.
+            </p>
+          </td>
+        </tr>
+      </table>
+
+      <Header />
+      <Footer />
     </div>
   );
 };
-
-
--- frontend/src/components/hero/Collection.jsx
+++ frontend/src/components/hero/Collection.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import img1 from "../../assets/image1.png";
 import img2 from "../../assets/image2.png";
 import img3 from "../../assets/image3.png";
@@ -27,39 +27,39 @@ const Collection = () => {
   ]);
 
   return (
-    <div className='min-h-screen flex flex-col items-center justify-center bg-gray-100 py-20'>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 py-20">
       <h1 className="text-4xl font-bold text-gray-600 mb-10 text-center">
         LATEST COLLECTION
       </h1>
-      
-      <div className='grid grid-cols-3 gap-11'>
+
+      <div className="grid grid-cols-3 gap-11 ml-10 mr-10">
         {products.map((product) => (
-          <div key={product.id} className='text-center'>
+          <div key={product.id} className="text-center">
             <img
               src={product.img}
               alt={product.name}
-              className='transform hover:scale-105 transition-transform duration-300'
+              className="transform hover:scale-105 transition-transform duration-300"
             />
-            <p className='mt-2 text-gray-600'>
+            <p className="mt-2 text-gray-600">
               {product.name} <br /> {product.price}
             </p>
           </div>
         ))}
       </div>
-      
-      <h1 className='font-bold text-4xl text-gray-600 mb-12 text-center mt-10'>
+
+      <h1 className="font-bold text-4xl text-gray-600 mb-12 text-center mt-10">
         BEST SELLERS
       </h1>
-      
-      <div className='grid grid-cols-4 gap-11'>
+
+      <div className="grid grid-cols-4 gap-11 ml-10 mr-10">
         {bestSellers.map((product) => (
-          <div key={product.id} className='text-center'>
+          <div key={product.id} className="text-center">
             <img
               src={product.img}
               alt={product.name}
-              className='transform hover:scale-105 transition-transform duration-300'
+              className="transform hover:scale-105 transition-transform duration-300 "
             />
-            <p className='mt-2 text-gray-600'>
+            <p className="mt-2 text-gray-600">
               {product.name} <br /> {product.price}
             </p>
           </div>
